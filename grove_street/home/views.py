ca1b7c43bbf58ab7eed262d401898d3bc5c86e7d
Riku Kaipainen <riku.kaipainen@gmail.com>
2024-04-15 10:40:45 +0300

    Created views for the new navigation

--- grove_street/home/views.py
+++ grove_street/home/views.py
@@ -2,4 +2,13 @@ from django.shortcuts import render
 from django.http import HttpRequest
 
 def home(request: HttpRequest):
-    return render(request, "home/index.html")
+    return render(request, "home/home.html")
+
+def about_me(request: HttpRequest):
+    return render(request, "home/about.html")
+
+def my_background(request: HttpRequest):
+    return render(request, "home/background.html")
+
+def miscellaneous(request: HttpRequest):
+    return render(request, "home/miscellaneous.html")
