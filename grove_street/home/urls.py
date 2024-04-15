5097a8e312a333e35099f3256e48bb7a4ffd1e69
Riku Kaipainen <riku.kaipainen@gmail.com>
2024-04-15 10:40:45 +0300

    Created views for the new navigation

--- grove_street/home/urls.py
+++ grove_street/home/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 urlpatterns = [
     path("", views.home, name="home"),
+    path("about", views.about_me, name="about"),
+    path("background", views.my_background, name="background"),
+    path("miscellaneous", views.miscellaneous, name="miscellaneous"),
 ]
