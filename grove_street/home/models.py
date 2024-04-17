81029761718d152f9362281e47d54d09d9b730a2
Riku Kaipainen <riku.kaipainen@gmail.com>
2024-04-17 12:28:42 +0300

    Make BlogPost model

--- grove_street/home/models.py
+++ grove_street/home/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class BlogPost(models.Model):
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
+    published_date = models.DateTimeField("Published date")
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+
+    class Meta:
+        permissions = [
+            ("can_publish", "Can publish new blog posts."),
+            ("can_edit", "Can modify existing blog posts."),
+            ("can_remove", "Can remove existing blog posts."),
+        ]
