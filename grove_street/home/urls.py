952cf2d1cde4f919a7363dffd312500ea4a10367
Riku Kaipainen <riku.kaipainen@gmail.com>
2024-04-18 15:55:37 +0300

    Simplified navigation to not include non-existent views

--- grove_street/home/urls.py
+++ grove_street/home/urls.py
@@ -4,7 +4,6 @@ from . import views
 
 urlpatterns = [
     path("", views.home, name="home"),
-    path("about", views.about_me, name="about"),
-    path("background", views.my_background, name="background"),
-    path("miscellaneous", views.miscellaneous, name="miscellaneous"),
+    path("about", views.about, name="about"),
+    path("blog", views.blog, name="blog"),
 ]
