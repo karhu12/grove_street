939e3118996220a74c6c45274457ef572f345c7f
Riku Kaipainen <riku.kaipainen@gmail.com>
2024-04-18 13:17:02 +0300

    Trying to display blog posts

--- grove_street/home/views.py
+++ grove_street/home/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpRequest
 
+from home.models import BlogPost
+
+# GET
+
 def home(request: HttpRequest):
-    return render(request, "home/home.html")
+    try:
+        latest_posts = BlogPost.objects.order_by("published_date")[:3]
+    except BlogPost.DoesNotExist:
+        latest_posts = None
+    return render(request, "home/home.html", {"latest_posts": latest_posts})
 
 def about_me(request: HttpRequest):
     return render(request, "home/about.html")
