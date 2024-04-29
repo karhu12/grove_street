2bab1722bdebf2484e16ff5bfa53acc39a3e16fd
Riku Kaipainen <riku.kaipainen@gmail.com>
2024-04-29 14:57:03 +0300

    Initial login/logout logic using django premade endpoints

--- grove_street/grove_street/settings.py
+++ grove_street/grove_street/settings.py
@@ -147,3 +147,6 @@ STATICFILES_DIRS = (
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# Redirect to home URL after login (Default redirects to /accounts/profile/)
+LOGIN_REDIRECT_URL = "/"
